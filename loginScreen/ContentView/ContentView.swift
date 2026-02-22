7f8a48836d0af205331e8600da419e7fa2a4d624
AtesiT <atesit@mail.ru>
2026-02-22 12:58:29 +0300

    ADD: button to change value of a button

---
+++ loginScreen/ContentView/ContentView.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var timer = 3
+    
+    var body: some View {
+        VStack {
+            Text(timer.formatted())
+                .font(.largeTitle)
+                .padding(.top, 100)
+            
+            Spacer()
+            
+            ButtonView(timer: $timer)
+            
+            Spacer()
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct ButtonView: View {
+    @Binding var timer: Int
+    
+    var body: some View {
+        Button("Start") {
+            timer -= 1
+        }
+    }
+}
