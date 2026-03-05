f1a75852228d4a3eb1f1a1b0267307948a23b840
AtesiT <atesit@mail.ru>
2026-03-05 15:55:20 +0300

    ADD: Note about Wrappers

--- loginScreen/loginScreenApp.swift
+++ loginScreen/loginScreenApp.swift
@@ -19,3 +19,10 @@ struct loginScreenApp: App {
         .environmentObject(loginViewVM)
     }
 }
+
+//  MARK: - NOTE ABOUT WRAPPERS
+
+//  @StateObject - обозначить экземпляр класса обновляемым. Предварительно до этого, необходимо подписать класс под ObservableObject. И создать в нём константу objectWillChange с ObservableObjectPublisher()
+//  @EnvironmentObject - обратиться к свойству и получить доступ из любого места (ссылка на окружение, в котором хранится этот объект)
+//  @ObservedObject - это как Binding, только для работы с классами, что подписаны под протокол ObservableObject
+//  @Published - позволяет следить за состоянием свойства. (Предварительно класс необходимо подписать под протокол ObservableObject.
