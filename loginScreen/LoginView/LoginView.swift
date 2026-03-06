a3cb3678dab222c082472377751fd6b7007d8876
AtesiT <atesit@mail.ru>
2026-03-06 13:41:47 +0300

    EDIT: loginView was rewrited (MADE: TextField)

--- loginScreen/LoginView/LoginView.swift
+++ loginScreen/LoginView/LoginView.swift
@@ -1,44 +1,27 @@
 import SwiftUI
 
 struct LoginView: View {
+    //  Создаём экземпляр класса loginViewViewModel
     @EnvironmentObject private var loginViewVM: LoginViewViewModel
-    @State private var showAlert = false
     
     var body: some View {
-        VStack {
-            //  Используем ZStack, чтобы разместить элементы над друг другом и используем .trailing к нему, чтобы прижать вправо. Из-за того, что TextField занимает всю ширину, он прижмется влево, а Text прижмтся вправо.
-            ZStack(alignment: .trailing) {
-                TextField("Type your name...", text: $loginViewVM.name)
-                //  Устанавливаем padding, чтобы тем самым сузить textField
-                    .padding(.horizontal, 50)
-                //  Размещаем по центру
-                    .multilineTextAlignment(.center)
-                Text("\(loginViewVM.name.count)")
-                //  Устанавилваем padding справа, это отодвинет наш объект влево
-                    .padding(.trailing, 25)
-                //  Терарный оператор для измнения цвета счётчика
-                    .foregroundStyle(loginViewVM.name.count >= 3 && loginViewVM.name.count < 20 ? .green : .red)
-            }
-            .padding(.bottom, 15)
-            Button(action: login) {
-                Label("OK", systemImage: "checkmark.circle")
-                    .foregroundStyle(loginViewVM.name.count >= 3 && loginViewVM.name.count < 20 ? .blue : .gray)
-            }
-        }
-        .alert("Ошибка", isPresented: $showAlert) {
-            Button("ОК", role: .cancel) {}
-        } message: {
-            Text("Имя пользователя не должно быть короче 3-ёх символов")
-        }
     }
+}
+
+struct TextFieldView: View {
+    @ObservedObject var loginViewVM: LoginViewViewModel
     
-    private func login() {
-        if loginViewVM.name.count >= 3 {
-            StorageManager.shared.saveData(userName: loginViewVM.name)
-            loginViewVM.isLoggedIn = true
-        } else {
-            showAlert = true
+    var body: some View {
+        TextField("Type your name...", text: $loginViewVM.user.name)
+            .multilineTextAlignment(.center)
+        HStack {
+            Spacer()
+            Text(loginViewVM.userNameCharCount)
+                .font(.callout)
+                .foregroundStyle(loginViewVM.nameIsValid ? .green : .red)
+                .padding([.top, .trailing])
         }
+        .padding(.bottom)
     }
 }
 
