d050c171ff85d3888d2810ea8bf411f65b64480f
AtesiT <atesit@mail.ru>
2026-03-06 13:14:20 +0300

    EDIT: File LoginViewVM (all logic was moved in the file)

--- loginScreen/LoginView/LoginViewViewModel.swift
+++ loginScreen/LoginView/LoginViewViewModel.swift
@@ -2,5 +2,32 @@ import SwiftUI
 import Combine
 
 final class LoginViewViewModel: ObservableObject {
-//    @AppStorage("isLoggedIn") var isLoggedIn = false
+    @Published var user = User()
+    
+    var nameIsValid: Bool {
+        user.name >= 3
+    }
+    
+    var userNameCharCount: String {
+        user.name.count.formatted()
+    }
+    
+    //  Определяем приватное свойство, после всех публичных свойств
+    private let storageManager = StorageManager.shared
+    
+    init(user: User = User()) {
+        self.user = user
+    }
+    
+    func login() {
+        user.isLoggedIn.toggle()
+        storageManager.save(user: user)
+    }
+    
+    func logout() {
+        user.name = ""
+        user.isLoggedIn.toggle()
+        //  Удаляем объект из памяти
+        storageManager.clear()
+    }
 }
